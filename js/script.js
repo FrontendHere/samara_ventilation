8615f449ccc9d03793e6b52082e7240dbaf3db4f
Iamfrontend <iamfrontend@mail.ru>
2017-10-04 13:21:23 +0500

    map

--- js/script.js
+++ js/script.js
@@ -30,4 +30,33 @@
 	 		}
 	 	}
 	 });
+	 //Карусель сертификатов
+	 $('.sertificates__owl').owlCarousel({
+	 	loop: true,	 	
+	 	dots: false,
+	 	navText: [],
+	 	responsive:{
+	 		0:{
+	 			items: 1,
+	 			nav: false
+	 		},
+	 		470:{
+	 			items: 1,
+	 			nav: true
+	 		},
+	 		768:{
+	 			items: 2,
+	 			nav: true
+	 		},
+	 		992:{
+	 			items: 3,
+	 			nav: true
+	 		},
+	 		1200:{
+	 			items: 4,
+	 			nav: true
+	 		}
+	 	}
+	 	
+	 });
  });
